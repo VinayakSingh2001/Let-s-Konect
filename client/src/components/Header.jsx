78ea3df97af82c37f19fbebfd098cfd93ebdbe81
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-01 22:39:02 +0530

    updated frontedend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
             <div className="flex pt-10 pb-10 w-full ml-14 ">
                 <div className='flex gap-3'>
                     <button className='"bg-black rounded-lg  hover:bg-white border border-gray-300'>
-                        <h3 className='text-white  hover:text-black text-xs pl-4 pr-4'>All</h3>
+                        <h3 className='text-white  text-xs pl-4 pr-4'>All</h3>
                     </button>
                     <button className='bg-white rounded-md hover:bg-gray-300 hover:text-black'>
                         <h3 className='text-black text-xs pl-4 pr-4'>AI Training</h3>
