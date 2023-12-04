c477654cfd2a70b027ee976e2871724fe864aa0f
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-05 02:12:20 +0530

    Creating a JWT & link to simulate scheduling

--- backend/expressRoutes.js
+++ backend/expressRoutes.js
@@ -1,5 +1,23 @@
 //this is where our express stuff happens
 const app = require("./server").app;
-app.get("/test", (req, res) => {
-  res.json("this is a test route");
+const jwt = require("jsonwebtoken");
+const linkSecret = "jbkjcnsdlkcmslcmsdc";
+
+app.get("/user-link", (req, res) => {
+  const appData = {
+    professionalsFullName: "Robert Down, J.D",
+    appointmentDate: Date.now(),
+  };
+
+  //we need to encode this data into a token
+  const token = jwt.sign(appData, linkSecret);
+  res.send("https://localhost:3000/join-video?token=" + token);
+
+  //   res.json("This is a test route");
+});
+
+app.get("/validate-link", (req, res) => {
+  const token = req.query.token;
+  const decodedData = jwt.verify(token, linkSecret);
+  res.json(decodedData);
 });
