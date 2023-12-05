cbbce1e91bc945f5c5ac449f008059c9656f3c52
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-06 02:12:20 +0530

    Add React-Router for our front-end

---
+++ frontend/src/videoComponents/MainVideoPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const MainVideoPage = () => {
+  return (
+    <h1>Main video page</h1>
+  );
+};
+
+export default MainVideoPage;
