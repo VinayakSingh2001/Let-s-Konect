9d4a2919f78b8e96334059c6dc9b53351ab67b82
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-07 02:12:20 +0530

    code updated

--- backend/index.js
+++
@@ -1,2 +0,0 @@
-require("./socketServer");
-require("./expressRoutes");
--- backend/socketServer.js
+++
@@ -1,7 +0,0 @@
-//all our socket server stuff happens here
-
-const io = require("./server").io;
-
-io.on("connection", (socket) => {
-  console.log(socket.id, "has connected");
-});
