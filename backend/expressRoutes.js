07cc011cf2f52b6fefd6e4cbddd64606eacbcd3b
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-09 02:12:20 +0530

    Add React-Router for our front-end

--- backend/expressRoutes.js
+++ backend/expressRoutes.js
@@ -1,6 +1,25 @@
 //here all our express stuff happens (routes)
 const app = require("./server").app;
+const jwt = require("jsonwebtoken");
+const linkSecret = "jbkjcnsdlkcmslcmsdc";
 
-app.get("/test", (req, res) => {
-  res.json("this is a route");
+//this route is for us!
+app.get("/user-link", (req, res) => {
+  //data for the end-user's appt
+  const apptData = {
+    professionalsFullName: "Robert Down, J.D",
+    appointmentDate: Date.now(),
+  };
+
+  //we need to encode this data in a token
+  const token = jwt.sign(apptData, linkSecret);
+  res.send("https://localhost:3000/join-video?token=" + token);
+
+  //   res.json("this is a route");
+});
+
+app.get("/validate-link", (req, res) => {
+  const token = req.query.token;
+  const decodedData = jwt.verify(token, linkSecret);
+  res.json(decodedData);
 });
---
+++ frontend/src/videoComponents/MainVideoPage.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+
+const MainVideoPage = () => {
+  return (
+    <div>
+      Main video page
+    </div>
+  )
+}
+
+export default MainVideoPage
